c9fb569a45efc2e2a90caea2f49d24270b87809c
jawad201-cmd <jawadprod1999@gmail.com>
2025-08-27 14:47:36 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-// src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
 );
-
